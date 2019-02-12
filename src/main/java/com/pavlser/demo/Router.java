126e693cc77e51141c1f2782fbb35d60765cf000
pavlser <sergiy.v.pavlenko@gmail.com>
2019-02-12 22:56:43 +0200

    Add simple route builder

--- src/main/java/com/pavlser/demo/Router.java
+++ src/main/java/com/pavlser/demo/Router.java
@@ -1,35 +1,77 @@
 package com.pavlser.demo;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
-import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
-import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.HandlerFunction;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.pavlser.demo.product.ProductHandler;
 
+import reactor.core.publisher.Mono;
+
 @Configuration
 public class Router {
 
+	List<String> registeredPath;
+	
 	@Bean
 	public RouterFunction<ServerResponse> route(ProductHandler handler) {
-		return RouterFunctions
-			.route(GET("/"), req -> ServerResponse.ok()
-				.contentType(MediaType.TEXT_HTML)
-				.body(BodyInserters.fromObject("Hello, Spring Reactor!")))
-			.andRoute(GET("/api/product/{id}")
-				.and(RequestPredicates.accept(MediaType.TEXT_PLAIN)), handler::getById)
-			.andRoute(POST("/api/product")
-				.and( accept(APPLICATION_JSON)), handler::createProduct);
+		SimpleRouteBuilder builder = new SimpleRouteBuilder()
+			.get("/", this::welcome)
+			.get("/api/v1/products", handler::allProducts)
+			.get("/api/v1/product/{id}", handler::getById)
+			.post("/api/v1/product", handler::createProduct);
+		registeredPath = builder.getPath();
+		return builder.build();
+	}
+	
+	public Mono<ServerResponse> welcome(ServerRequest request) {
+		String s = "Hello from Spring Reactor!\n\n"
+			+ "Please check urls:\n"
+			+ String.join("\nhttp://localhost:8090", registeredPath);
+		return ServerResponse.ok()
+			.contentType(MediaType.TEXT_PLAIN)
+			.body(BodyInserters.fromPublisher(Mono.just(s), String.class));
+	}
+	
+}
+
+class SimpleRouteBuilder {
+	
+	RouterFunctions.Builder builder;
+	List<String> paths;
+	
+	public SimpleRouteBuilder() {
+		builder = RouterFunctions.route();
+		paths = new ArrayList<>();
+	}
+	
+	public SimpleRouteBuilder get(String path, HandlerFunction<ServerResponse> handler) {
+		builder.GET(path, handler);
+		paths.add(path);
+		return this;
+	}
+	
+	public SimpleRouteBuilder post(String path, HandlerFunction<ServerResponse> handler) {
+		builder.POST(path, handler);
+		paths.add(path);
+		return this;
+	}
+	
+	public RouterFunction<ServerResponse> build() {
+		return builder.build();
+	}
+	
+	public List<String> getPath() {
+		return paths;
 	}
 	
 }
--- src/main/java/com/pavlser/demo/product/ProductHandler.java
+++ src/main/java/com/pavlser/demo/product/ProductHandler.java
@@ -1,11 +1,13 @@
 package com.pavlser.demo.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -13,17 +15,27 @@ public class ProductHandler {
 	
 	@Autowired
 	ProductReactiveRepository repository;
+	
+	public Mono<ServerResponse> allProducts(ServerRequest request) {
+		Flux<Product> products = repository.findAll();
+		return ServerResponse.ok()
+			.contentType(MediaType.APPLICATION_JSON)
+			.body(products, Product.class);
+	}
 
 	public Mono<ServerResponse> getById(ServerRequest request) {
 		String id = request.pathVariable("id");
 		return repository.findFirstById(id)
-			.flatMap(product -> ServerResponse.ok().body(Mono.just(product), Product.class))
+			.flatMap(product -> 
+				ServerResponse.ok()
+				.contentType(MediaType.APPLICATION_JSON)
+				.body(Mono.just(product), Product.class))
 			.switchIfEmpty(ServerResponse.notFound().build());
 	}
 	
 	public Mono<ServerResponse> createProduct(ServerRequest request) {
 		return ServerResponse.ok()
-			//.contentType(MediaType.APPLICATION_JSON)
+			.contentType(MediaType.APPLICATION_JSON)
 			.body(BodyInserters.fromPublisher(Mono.just("OK"), String.class));
 	}
 
